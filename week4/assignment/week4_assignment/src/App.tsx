7af704f0d91d8ecceaa25b6f832a0c2010bb8de4
ayla_12@naver.com <ayla_12@naver.com>
2024-11-11 17:49:45 +0900

    Feat: 라우터 구성

--- week4/assignment/week4_assignment/src/App.tsx
+++ week4/assignment/week4_assignment/src/App.tsx
@@ -5,13 +5,28 @@ import { theme } from './styles/theme'
 
 import './App.css'
 
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import LogIn from './pages/LogIn'
+import SignUp from './pages/SignUp'
+import MyPage from './pages/MyPage'
+
+
+
+
 
 function App() {
 
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-
+      <BrowserRouter>
+        <Routes>
+          {/* 로그인 페이지 */}
+          <Route path="/" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/mypage" element={<MyPage />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
